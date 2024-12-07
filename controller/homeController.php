2ae5caac8a738931771a98d54230afcc6ddbd61d
Thomas <thomas.brionesm@gmail.com>
2024-12-07 03:31:19 -0300

    Crear validacion y manejar excepcion del rut, validarRut y validarRutChileno se movio de homeController a homeModel

--- controller/homeController.php
+++ controller/homeController.php
@@ -29,31 +29,5 @@
             $keydb = $this->MODEL->obtenerclave($correo);
             return (password_verify($password, $keydb)) ? true : false;
         }
-
-        public function validarRut($rut) {
-            $rut = str_replace([".", "-"], "", $rut);
-            if (preg_match("/^[0-9]{7,8}[0-9Kk]{1}$/", $rut)) {
-                return $this->validarRutChileno($rut);
-            }
-            return false;
-        }
-        
-        private function validarRutChileno($rut) {
-            $rutSinDV = substr($rut, 0, -1);
-            $dv = strtoupper(substr($rut, -1));
-            $suma = 0;
-            $factor = 2;
-            for ($i = strlen($rutSinDV) - 1; $i >= 0; $i--) {
-                $suma += $rutSinDV[$i] * $factor;
-                $factor = ($factor == 7) ? 2 : $factor + 1;
-            }
-            $dvCalculado = 11 - ($suma % 11);
-            if ($dvCalculado == 11) {
-                $dvCalculado = '0';
-            } elseif ($dvCalculado == 10) {
-                $dvCalculado = 'K';
-            }
-            return $dv == $dvCalculado;
-        }
     }
 ?>
--- model/homeModel.php
+++ model/homeModel.php
@@ -7,11 +7,16 @@
             $pdo = new db();
             $this->PDO = $pdo->conexion();
         }
+
         public function agregarNuevoUsuario($correo,$password,$rut){
             $statement = $this->PDO->prepare("INSERT INTO usuarios (correo, rut, `PASSWORD`) VALUES (:correo, :rut, :password)");
             $statement->bindParam(":correo",$correo);
             $statement->bindParam(":rut",$rut);
             $statement->bindParam(":password",$password);
+            
+            if (!$this->validarRut($rut)) {
+                return false;
+            }
             try {
                 $statement->execute();
                 return true;
@@ -23,6 +28,31 @@
             $statement = $this->PDO->prepare("SELECT PASSWORD FROM usuarios WHERE correo = :correo");
             $statement->bindParam(":correo",$correo);
             return ($statement->execute()) ? $statement->fetch()['PASSWORD'] : false;
+        }
+        public function validarRut($rut) {
+            $rut = str_replace([".", "-"], "", $rut);
+            if (preg_match("/^[0-9]{7,8}[0-9Kk]{1}$/", $rut)) {
+                return $this->validarRutChileno($rut);
+            }
+            return false;
+        }
+        
+        private function validarRutChileno($rut) {
+            $rutSinDV = substr($rut, 0, -1);
+            $dv = strtoupper(substr($rut, -1));
+            $suma = 0;
+            $factor = 2;
+            for ($i = strlen($rutSinDV) - 1; $i >= 0; $i--) {
+                $suma += $rutSinDV[$i] * $factor;
+                $factor = ($factor == 7) ? 2 : $factor + 1;
+            }
+            $dvCalculado = 11 - ($suma % 11);
+            if ($dvCalculado == 11) {
+                $dvCalculado = '0';
+            } elseif ($dvCalculado == 10) {
+                $dvCalculado = 'K';
+            }
+            return $dv == $dvCalculado;
         }
     }
 
